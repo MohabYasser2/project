a0bd6989f247bfaf9afa424102ab1e97a2919aff
MohabYasser2 <mohabhelp@gmail.com>
2024-05-18 21:30:56 +0300

    aaa

--- Code/project/EXECUTE.vhd
+++ Code/project/EXECUTE.vhd
@@ -13,6 +13,8 @@ Read_Data2: IN std_logic_vector(31 DOWNTO 0);
 IMM_VALUE: IN std_logic_vector(31 DOWNTO 0);
 RTI_SIG: IN std_logic;
 RTI_FLAGS: IN std_logic_vector(3 DOWNTO 0);
+IN_SIG: IN std_logic;
+IN_PORT: IN  std_logic_vector(31 DOWNTO 0);
 ALU_OUTPUT: OUT  std_logic_vector(31 DOWNTO 0);
 FLAGS: OUT std_logic_vector(3 downto 0);
 PREV_FLAGS: OUT std_logic_vector(3 downto 0);
@@ -60,6 +62,7 @@ END component;
 Signal MUX1OUT : std_logic_vector(31 DOWNTO 0) := (others => '0');
 Signal MUX2OUT : std_logic_vector(31 DOWNTO 0) := (others => '0');
 Signal MUX3OUT : std_logic_vector(31 DOWNTO 0) := (others => '0');
+SIGNAL MUX4OUT : STD_LOGIC_VECTOR(31 DOWNTO 0) := (OTHERS => '0');
 signal temp_flags : std_logic_vector(3 downto 0) := (others => '0');
 signal temp_flags1 : std_logic_vector(3 downto 0) := (others => '0');
 BEGIN
@@ -68,7 +71,8 @@ U3:Mux2x1 GENERIC MAP (32) PORT MAP(Read_data1,ForwardedData ,Forwardselector1,M
 U4:Mux2x1 GENERIC MAP (32) PORT MAP(read_data2,ForwardedData ,Forwardselector2,MUX2OUT);
 
 U0:Mux2x1 GENERIC MAP (32) PORT MAP(MUX2OUT,imm_value ,ALU_SRC,MUX3OUT);
-U1:ALU PORT MAP(CLK,RST,MUX1OUT,MUX3OUT,OP_CODE,temp_flags,AlU_OUTPUT,temp_flags1,EXCEPTION_O);
+U5:Mux2x1 GENERIC MAP (32) PORT MAP(MUX3OUT,IN_PORT,IN_SIG,MUX4OUT);
+U1:ALU PORT MAP(CLK,RST,MUX1OUT,MUX4OUT,OP_CODE,temp_flags,AlU_OUTPUT,temp_flags1,EXCEPTION_O);
 U2:CCR Port MAP(CLK,RST,RTI_SIG,temp_flags1,RTI_FLAGS,temp_flags);
 
 FLAGS <= temp_flags1;
--- Code/project/ForwardingUnit.vhd
+++ Code/project/ForwardingUnit.vhd
@@ -3,49 +3,43 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY ForwardingUnit IS
-PORT (
-    clk,SWAPPING1,SWAPPING2,REGWRITE1,REGWRITE2 : IN STD_LOGIC;
-    Src1, Src2, ExDst, MemDst : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
-    MemtoRegE, MemtoRegM : IN STD_LOGIC;
-    ExResultMe, MemResult : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
-    ForwardedData1 : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
-    Selector1, Selector2 : OUT STD_LOGIC
+    PORT (
+        clk,SWAPPING1,SWAPPING2,REGWRITE1,REGWRITE2 : IN STD_LOGIC;
+        Src1, Src2, ExDst, MemDst : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
+        MemtoRegE, MemtoRegM : IN STD_LOGIC;
+        ExResultMe, MemResult : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
+        ForwardedData1 : OUT STD_LOGIC_VECTOR (31 DOWNTO 0);
+        Selector1, Selector2 : OUT STD_LOGIC
     );
-    END ForwardingUnit;
-    
-    ARCHITECTURE Forwarding OF ForwardingUnit IS
-    
-    signal SAVED_DATA1 : STD_LOGIC_VECTOR (31 DOWNTO 0) := (OTHERS => '0');
-    signal SAVED_DATA2 : STD_LOGIC_VECTOR (31 DOWNTO 0) := (OTHERS => '0');
+END ForwardingUnit;
+
+ARCHITECTURE Forwarding OF ForwardingUnit IS
+
 begin
     PROCESS (clk)
     VARIABLE S1,S2 : STD_LOGIC := '0';
     variable ForwardedData : STD_LOGIC_VECTOR (31 DOWNTO 0) := (OTHERS => '0');
     BEGIN
-    if falling_edge(clk) then
-        SAVED_DATA1 <= exResultMe;
-        SAVED_DATA2 <= memResult;
-    end if;    
-    IF RISING_EDGE(clk) THEN
+        IF RISING_EDGE(clk) THEN
             -- Default selectors to '0
             S1 := '0';
             S2 := '0';
             ForwardedData := (others => '0');
             -- Forwarding for Src1
             IF Src1 = MemDst AND MemtoRegM = '0' AND SWAPPING2 = '0' AND REGWRITE1 ='1'THEN
-                ForwardedData := Saved_Data2;
+                ForwardedData := MemResult;
                 S1 := '1';
             END if;
             IF Src1 = ExDst AND MemtoRegE = '1' AND SWAPPING1 = '0' AND REGWRITE2 ='1'THEN
-                ForwardedData := Saved_Data1;
+                ForwardedData := ExResultMe;
                 S1 := '1';
             END IF; 
             if Src2 = MemDst AND MemtoRegM = '0'AND SWAPPING2 = '0'AND REGWRITE1 ='1' THEN
-                ForwardedData := Saved_Data2;
+                ForwardedData := MemResult;
                 S2 := '1';
                 END if;
             IF Src2 = ExDst AND MemtoRegE = '1' AND SWAPPING1 = '0'AND REGWRITE2 ='1'THEN
-                ForwardedData := Saved_Data1;
+                ForwardedData := ExResultMe;
                 S2 := '1';
          
             END IF;
