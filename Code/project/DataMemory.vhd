6bd5eb7ad0d56983ba3e539c2c7b9c62067328d4
MohabYasser2 <mohabhelp@gmail.com>
2024-05-18 03:22:06 +0300

    alo

--- Code/project/DataMemory.vhd
+++ Code/project/DataMemory.vhd
@@ -21,28 +21,29 @@ ARCHITECTURE ARCH OF DataMemory IS
 TYPE ram_type IS ARRAY(4095 DOWNTO 0) of std_logic_vector(16 DOWNTO 0);
 SIGNAL RAM : ram_type ;
 SIGNAL Protect_Bit : std_logic;
+SIGNAL TEMP_DATAOUT : std_logic_vector(31 DOWNTO 0);
 BEGIN
 
 PROCESS(CLK, RST) IS
 BEGIN
 
 if Protect_SIG = '1' then
-RAM(to_integer(unsigned(ADDRESS(15 DOWNTO 0))) )(16) <= '1';
-RAM(to_integer(unsigned(ADDRESS(15 DOWNTO 0))) + 1 )(16) <= '1';
+RAM(to_integer("0000" & unsigned(ADDRESS(11 DOWNTO 0))) )(16) <= '1';
+RAM(to_integer("0000" & unsigned(ADDRESS(11 DOWNTO 0))) + 1 )(16) <= '1';
 end if;
 if Free_SIG = '1' then
-RAM(to_integer(unsigned(ADDRESS(15 DOWNTO 0))) )(16) <= '0';
-RAM(to_integer(unsigned(ADDRESS(15 DOWNTO 0))) + 1 )(16) <= '0';
+RAM(to_integer("0000" & unsigned(ADDRESS(11 DOWNTO 0))) )(16) <= '0';
+RAM(to_integer("0000" & unsigned(ADDRESS(11 DOWNTO 0))) + 1 )(16) <= '0';
 end if;   
 
 
 Protect_Bit <= RAM(to_integer(unsigned(ADDRESS(15 DOWNTO 0))))(16);
 
 IF INTERRUPT_SIG = '1' THEN
-RAM(to_integer(unsigned(ADDRESS(15 DOWNTO 0))) ) <= '0' & WriteData(31 DOWNTO 16);
-RAM(to_integer(unsigned(ADDRESS(15 DOWNTO 0))) + 1 ) <='0'&  WriteData(15 DOWNTO 0);
-RAM(to_integer(unsigned(ADDRESS(15 DOWNTO 0))) - 2) <= "0000000000000" & FLAGS(3 DOWNTO 0);
-RAM(to_integer(unsigned(ADDRESS(15 DOWNTO 0))) - 1 ) <="00000000000000000" ;
+RAM(to_integer("0000" & unsigned(ADDRESS(11 DOWNTO 0))) ) <= '0' & WriteData(31 DOWNTO 16);
+RAM(to_integer("0000" & unsigned(ADDRESS(11 DOWNTO 0))) + 1 ) <='0'&  WriteData(15 DOWNTO 0);
+RAM(to_integer("0000" & unsigned(ADDRESS(11 DOWNTO 0))) - 2) <= "0000000000000" & FLAGS(3 DOWNTO 0);
+RAM(to_integer("0000" & unsigned(ADDRESS(11 DOWNTO 0))) - 1 ) <="00000000000000000" ;
 END IF;
 
 IF RTI_SIG = '1' THEN
@@ -54,15 +55,21 @@ IF RST = '1' THEN
 RAM <= (OTHERS => (OTHERS => '0'));
 
 ELSIF falling_edge(CLK) THEN
-IF WE = '1'  AND Protect_bit = '0' THEN
-RAM(to_integer(unsigned(ADDRESS(15 DOWNTO 0))) ) <= '0' & WriteData(31 DOWNTO 16);
-RAM(to_integer(unsigned(ADDRESS(15 DOWNTO 0))) + 1 ) <='0'&  WriteData(15 DOWNTO 0);
-END IF;
-END IF;
+    IF WE = '1'  AND Protect_bit = '0' THEN
+    RAM(to_integer("0000" & unsigned(ADDRESS(11 DOWNTO 0))) ) <= '0' & WriteData(31 DOWNTO 16);
+    RAM(to_integer("0000" & unsigned(ADDRESS(11 DOWNTO 0))) + 1 ) <='0'&  WriteData(15 DOWNTO 0);
+    END IF;
+
+    IF ADDRESS(11 DOWNTO 0) = "111111111111" THEN
+    TEMP_DATAOUT <= "0000000000000000";
+    else
+    TEMP_DATAOUT <=RAM(to_integer("0000" & unsigned(ADDRESS(11 DOWNTO 0 ))))(15 downto 0) & RAM(to_integer("0000" & unsigned(ADDRESS(11 DOWNTO 0) )) + 1)(15 downto 0) ;
+    END IF;
+    END IF;
 
 END PROCESS;
 
-DATAOUT <= RAM(to_integer(unsigned(ADDRESS(15 DOWNTO 0 ))))(15 downto 0) & RAM(to_integer(unsigned(ADDRESS(15 DOWNTO 0) )) + 1)(15 downto 0) ;
 
+DATAOUT <= TEMP_DATAOUT;
 
 END ARCHITECTURE;
