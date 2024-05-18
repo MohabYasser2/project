956d466397a5cdd9beae1ed2623005df282ef1d5
MohabYasser2 <mohabhelp@gmail.com>
2024-05-18 16:31:31 +0300

    aaa

--- Code/project/ForwardingUnit.vhd
+++ Code/project/ForwardingUnit.vhd
@@ -4,7 +4,7 @@ USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY ForwardingUnit IS
     PORT (
-        clk : IN STD_LOGIC;
+        clk,SWAPPING1,SWAPPING2 : IN STD_LOGIC;
         Src1, Src2, ExDst, MemDst : IN STD_LOGIC_VECTOR (2 DOWNTO 0);
         MemtoRegE, MemtoRegM : IN STD_LOGIC;
         ExResultMe, MemResult : IN STD_LOGIC_VECTOR (31 DOWNTO 0);
@@ -14,37 +14,40 @@ ENTITY ForwardingUnit IS
 END ForwardingUnit;
 
 ARCHITECTURE Forwarding OF ForwardingUnit IS
+signal S1,S2 : STD_LOGIC := '0';
 BEGIN
     PROCESS (clk)
     BEGIN
         IF rising_edge(clk) THEN
-            -- Default selectors to '0'
-            Selector1 <= '0';
-            Selector2 <= '0';
+            -- Default selectors to '0
 
             -- Forwarding for Src1
-            IF Src1 = MemDst AND MemtoRegM = '0' THEN
+            IF Src1 = MemDst AND MemtoRegM = '0' AND SWAPPING2 = '0' THEN
                 ForwardedData1 <= MemResult;
-                Selector1 <= '1';
-            ELSIF Src1 = ExDst AND MemtoRegE = '0' THEN
+                S1 <= '1';
+            ELSIF Src1 = ExDst AND MemtoRegE = '1' AND SWAPPING1 = '0'THEN
                 ForwardedData1 <= ExResultMe;
-                Selector1 <= '1';
+                S1 <= '1';
             ELSE
                 -- Default case if no forwarding is needed
+                S1 <= '0';
                 ForwardedData1 <= (others => '0');
             END IF;
 
             -- Forwarding for Src2
-            IF Src2 = MemDst AND MemtoRegM = '0' THEN
+            IF Src2 = MemDst AND MemtoRegM = '0'AND SWAPPING2 = '0' THEN
                 ForwardedData1 <= MemResult;
-                Selector2 <= '1';
-            ELSIF Src2 = ExDst AND MemtoRegE = '0' THEN
+                S2 <= '1';
+            ELSIF Src2 = ExDst AND MemtoRegE = '1' AND SWAPPING1 = '0'THEN
                 ForwardedData1 <= ExResultMe;
-                Selector2 <= '1';
+                S2 <= '1';
             ELSE
                 -- Default case if no forwarding is needed
+                S2 <= '0';
                 ForwardedData1 <= (others => '0');
             END IF;
         END IF;
     END PROCESS;
+    Selector1 <= S1;
+    Selector2 <= S2;
 END Forwarding;
